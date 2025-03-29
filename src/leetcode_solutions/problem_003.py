13dd8b3b6b1cff47186b55ee76925b5d681e6d4f
Ryan White <ryan.rlw.white@gmail.com>
2025-03-29 07:31:37 -0400

    Implement problem 3 via TDD

--- src/leetcode_solutions/problem_003.py
+++ src/leetcode_solutions/problem_003.py
@@ -1,3 +1,20 @@
+# Initial placeholder for the function
 def length_of_longest_substring(s: str) -> int:
-    len_longest_ss = len(s)
-    return len_longest_ss
+    """Finds the length of the longest substring without repeating characters."""
+    substring_lengths = [0] # there is always a substring of length zero
+
+    # generate candidate_substring (as a list of chars)
+    candidate_substring = [] 
+    for el in s:
+        if candidate_substring.count(el) > 0:
+            # we were at the end of the substring before reaching el
+            substring_lengths.append(len(candidate_substring))
+            
+            # prune everything up to previous instance of el from substring
+            candidate_substring = candidate_substring[candidate_substring.index(el) + 1:]
+
+        candidate_substring.append(el)
+        
+    substring_lengths.append(len(candidate_substring))
+
+    return max(substring_lengths)
--- tests/test_problem_003.py
+++ tests/test_problem_003.py
@@ -1,9 +1,30 @@
 import pytest
-from leetcode_solutions.problem_003_length_of_longest_substring import length_of_longest_substring as f
+from leetcode_solutions.problem_003 import length_of_longest_substring
 
+
+# Initial placeholder test (will fail because of NotImplementedError)
+# def test_placeholder():
+#     with pytest.raises(NotImplementedError):
+#         length_of_longest_substring(1)
 
 def test_empty_string():
-    assert f("") == 0
+    assert length_of_longest_substring("") == 0
+
 
 def test_single_char():
-    assert f("a") == 1
+    assert length_of_longest_substring("a") == 1
+    assert length_of_longest_substring(" ") == 1
+
+
+def test_all_unique_chars():
+    assert length_of_longest_substring("abcde") == 5
+    assert length_of_longest_substring("abcde ") == 6
+
+
+def test_my_tests():
+    assert length_of_longest_substring("aaa") == 1
+    assert length_of_longest_substring("aab") == 2
+    assert length_of_longest_substring("aba") == 2
+    assert length_of_longest_substring("abcde") == 5
+    assert length_of_longest_substring("abcde ") == 6
+    assert length_of_longest_substring("abcdeaaa ") == 5
